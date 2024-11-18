89aab2dd5a992cd4648201625a019dde484db7ff
Joaogabrielgabry <joaogabrielgabry@gmail.com>
2024-11-18 16:59:34 -0300

    global css incompleto

---
+++ WikiPokeDex/src/Global/GlobalCss.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export const GlobalCss = StyleSheet.create({
+    body: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        padding: 10,
+        backgroundColor: '#f2b807'
+    },
+});
---
+++ WikiPokeDex/src/Pages/DevelopersInfo/index.tsx
@@ -0,0 +1,3 @@
+// import React from 'react';
+// import { View } from 'react-native';
+// import { index } from '../../Components/Header';
