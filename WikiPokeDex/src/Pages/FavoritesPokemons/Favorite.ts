fc1a854bea8ba9c3269891dc9c4f4717e7f86a15
Letícia Peixoto <leticia_95lima@hotmail.com>
2024-11-19 16:13:40 -0300

    css favorites

--- WikiPokeDex/src/Pages/FavoritesPokemons/Favorite.ts
+++ WikiPokeDex/src/Pages/FavoritesPokemons/Favorite.ts
@@ -1,5 +1,17 @@
 import { StyleSheet } from "react-native";
 
 export const FavoriteStyle = StyleSheet.create({
-
+    body: {
+        flex: 1,
+        flexDirection: 'column',
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f2b807'
+    },
+    PrincipalContent:{
+        flex: 1,
+        flexDirection: 'column',
+        gap: 15,
+    }
 });
