05ae32e658f6c3427638fc96360bd4a35bf56d15
Bardasson <gabriel123.bardasson@gmail.com>
2024-11-21 19:35:17 -0300

    correção de import

--- WikiPokeDex/src/Pages/Login/index.tsx
+++ WikiPokeDex/src/Pages/Login/index.tsx
@@ -6,7 +6,7 @@ import { useNavigation } from '@react-navigation/native';
 import { NavigationProps } from '../../Routes/NavegationPage';
 import { Button } from '../../Components/ButtonForm';
 import { getLogin} from '../../Api/Register';
-import { AuthContext } from '../../Context/AuthContext';
+import { AuthContext } from '../../context/AuthContext';
 
 export function Login() {
     const [username, setUsername] = useState('');
